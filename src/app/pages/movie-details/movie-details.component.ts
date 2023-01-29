b9f12dfd266c732cf5c182422b9f710ea6675037
arthking17 <nguesseuarthur17@gmail.com>
2023-01-29 13:00:35 +0100

    -> Show movies by genre✅ -> Display movie details✅

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { MovieApiServiceService } from './../../service/movie-api-service.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
 
+  constructor(private movieDBService: MovieApiServiceService, private router: ActivatedRoute) {}
+  movieDetails: any;
+  movieVideo: any;
+  movieCast: any;
+
+  ngOnInit(): void {
+    let getParamId = this.router.snapshot.paramMap.get('id');
+    console.log(getParamId, 'movie_id#');
+
+    this.getMovieDetails(getParamId);    
+    this.getMovieVideos(getParamId);
+    this.getMovieCast(getParamId);
+  }
+
+  getMovieDetails(id:any) {
+    this.movieDBService.getMovieDetails(id).subscribe((result) => {
+      console.log(result, 'getMovieDetails#');
+      this.movieDetails = result;
+    })
+  }
+
+  getMovieVideos(id:any) {
+    this.movieDBService.getMovieVideos(id).subscribe((result) => {
+      console.log(result, 'getMovieVideos#');
+      result.results.forEach((element:any) => {
+        if(element.type == "Trailer") {
+          this.movieVideo = element.key;
+          console.log(this.movieVideo, 'getMovieVideoKey#');
+        }
+      });
+    })
+  }
+
+  getMovieCast(id:any) {
+    this.movieDBService.getMovieCredits(id).subscribe((result) => {
+      console.log(result, 'getMovieCredits#');
+      this.movieCast = result.cast;
+    })
+  }
 }
