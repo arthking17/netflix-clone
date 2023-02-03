401933f09c93be684eef27ec4ff77120e903bfc3
arthking17 <nguesseuarthur17@gmail.com>
2023-02-03 21:11:09 +0100

    Update list of movies genre in Home page✅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   navbg:any
 
   @HostListener('document:scroll') scrollover(){
-    console.log(document.body.scrollTop, 'scrolllength#');
+    //console.log(document.body.scrollTop, 'scrolllength#');
 
     if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
       this.navbg = {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,20 +13,42 @@ export class HomeComponent implements OnInit {
   bannerMovies: any = [];
   trendingMovies: any = [];
   movieGenres: any = [];
-  moviesByGenre: any[][] = [[],[]];
-
+  moviesByGenre: any[][] = [[], []];
+  topGenres: any = [{
+    "id": 28,
+    "name": "Action"
+  }, {
+    "id": 16,
+    "name": "Animation"
+  }, {
+    "id": 35,
+    "name": "Comedy"
+  }, {
+    "id": 99,
+    "name": "Documentary"
+  }, {
+    "id": 10749,
+    "name": "Romance"
+  }, {
+    "id": 878,
+    "name": "Science Fiction"
+  }, {
+    "id": 27,
+    "name": "Horror"
+  }];
 
   ngOnInit(): void {
     this.bannerData();
     this.trendingData();
 
-    this.listAllMoviesByGenre();
+    this.listMoviesOfTopGenres();
+    //this.listAllMoviesByGenre();
   }
 
   //bannerData
   bannerData() {
     this.MovieDBService.bannerApiData().subscribe((result) => {
-      console.log(result, 'bannerMovies#');
+      //console.log(result, 'bannerMovies#');
       this.bannerMovies = result.results;
 
     })
@@ -34,28 +56,28 @@ export class HomeComponent implements OnInit {
 
   trendingData() {
     this.MovieDBService.trendingMovieApiData().subscribe((result) => {
-      console.log(result, 'trendingMovies#');
+      //console.log(result, 'trendingMovies#');
       this.trendingMovies = result.results;
     })
   }
 
   listMovieGenres() {
     this.MovieDBService.getListGenreMovies().subscribe((result) => {
-      console.log(result, 'listMovieGenres#');
+      //console.log(result, 'listMovieGenres#');
       this.movieGenres = result.genres;
     })
   }
 
   listMoviesByGenre(genreMovie: any) {
     return this.MovieDBService.getListMoviesByGenre(genreMovie).subscribe((result) => {
-      console.log(result, 'listMoviesByGenre#');
+      //console.log(result, 'listMoviesByGenre#');
       this.moviesByGenre = result.results;
     })
   }
 
   listAllMoviesByGenre() {
     this.MovieDBService.getListGenreMovies().subscribe((result) => {
-      console.log(result, 'listMovieGenres#');
+      //console.log(result, 'listMovieGenres#');
 
       this.movieGenres = result.genres;
 
@@ -63,11 +85,22 @@ export class HomeComponent implements OnInit {
 
         this.MovieDBService.getListMoviesByGenre(genre.id).subscribe((result) => {
 
-          console.log(result, `${genre.name}#`);
+          //console.log(result, `${genre.name}#`);
           this.moviesByGenre[genre.id] = result.results;
         })
       });
     })
+  }
+
+  listMoviesOfTopGenres() {
+    this.topGenres.forEach((genre: any) => {
+
+      this.MovieDBService.getListMoviesByGenre(genre.id).subscribe((result) => {
+
+        //console.log(result, `${genre.name}#`);
+        this.moviesByGenre[genre.id] = result.results;
+      })
+    });
   }
 
 }
